c0cc744788637f138dc31d0b0d60ea97d81fa9b0
Maksim Zhukau <Maksim.Zhukau@itechart-group.com>
2018-12-14 14:23:14 +0300

    added react table filters

---
+++ reactTableFilters/precache-manifest.7dd2442169684f7af5f12e015b0847bc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "82f4118979f0de1d516a",
+    "url": "/static/css/main.62a51c3c.chunk.css"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "3e16b144d806dbe57fb6",
+    "url": "/static/js/1.3e16b144.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82f4118979f0de1d516a",
+    "url": "/static/js/main.82f41189.chunk.js"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/static/media/rw-widgets.eceddf47.ttf"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "3e16b144d806dbe57fb6",
+    "url": "/static/css/1.c89aee39.chunk.css"
+  },
+  {
+    "revision": "86e64fd6dcb919354f725036323044eb",
+    "url": "/index.html"
+  }
+];
